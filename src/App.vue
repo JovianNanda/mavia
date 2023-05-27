0a121a6a6c5c7102cb9d8b4f40ac8e2044095d37
MadeAditya02 <imadeaditya4@gmail.com>
2023-05-27 20:10:53 +0800

    Initialize repo

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      Hello Mavia
+      <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <button class="btn btn-primary">Submit</button>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+// import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap'
+import App from './App.vue'
+
+createApp(App).mount('#app')
